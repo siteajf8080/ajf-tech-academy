1dbaf6c5448a1fd3babee5cf2895375e9ffbeb39
Jofa <jonathanfrancoisalcena08@gmail.com>
2026-03-20 16:33:20 -0400

    Mise a jour de deux fichiers

--- config/settings.py
+++ config/settings.py
@@ -8,9 +8,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-28r_kco--lgm_hh%yfy!%yqfl8e663_(((#o!6erh3y)@$t6b!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# Nou mete l sou False pou pèfòmans ak sekirite sou Render
+DEBUG = False
 
-ALLOWED_HOSTS = []
+# Adrès ki gen pèmisyon pou ouvri sit la
+ALLOWED_HOSTS = ['ajf-tech-academy.onrender.com', 'ajftech.tech', 'www.ajftech.tech', 'localhost', '127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
@@ -19,6 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic', # Pou WhiteNoise jere static menm nan devlopman
     'django.contrib.staticfiles',
     
     # App pa ou la
@@ -27,6 +30,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', # Trè enpòtan pou Render
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -48,16 +52,17 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                # Sa a enpòtan pou foto yo (screenshots, profile pics) ka parèt nan templates yo
                 'django.template.context_processors.media',
             ],
         },
-    },
+    },    
 ]
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
+# Nòmalman sou Render w ap bezwen PostgreSQL pou done yo pa efase,
+# men pou kounye a n ap itilize SQLite jan w te genyen l lan.
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -75,37 +80,35 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 LANGUAGE_CODE = 'fr' 
-TIME_ZONE = 'America/Port-au-Prince' # Nou mete lè Ayiti pou tranzaksyon yo egzat
+TIME_ZONE = 'America/Port-au-Prince'
 USE_I18N = True
 USE_TZ = True
 
-# --- STATIC FILES (CSS, JS, Images pwojè a) ---
+# --- STATIC FILES (CSS, JS) ---
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
     BASE_DIR / "academy" / "static",
 ]
-# Kote Django ap rasanble tout static yo lè w ap mete l an liy (production)
+# Kote Render ap rasanble tout static yo
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-# --- MEDIA FILES (Foto elèv, Screenshot peman) ---
+# Konfigirasyon WhiteNoise pou konpresyon ak kach (cache)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# --- MEDIA FILES (Foto, Screenshots) ---
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # --- AUTHENTICATION CONFIG ---
-# Sa a di Django kote paj login lan ye pou @login_required
 LOGIN_URL = 'login' 
-
-# Kote itilizatè a prale apre li fin konekte
 LOGIN_REDIRECT_URL = 'dashboard'
-
-# Kote li prale apre li fin dekonekte
 LOGOUT_REDIRECT_URL = 'home'
 
 # --- DEFAULT AUTO FIELD ---
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# --- MESSAGE TAGS (Pou Bootstrap alerts) ---
+# --- MESSAGE TAGS ---
 from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.DEBUG: 'secondary',
